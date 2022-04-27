4cd24969b86731e659a1cf67618ace331ad585c3
Pooran Sen <pooransen50@gmail.com>
2022-04-27 18:54:11 +0530

    Category Completed

---
+++ src/components/NewsItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NewsItem = (props)=> {
+        let {title} = props;
+        return (
+             <div>
+               <h5 className="card-title">{title}</h5>
+            </div>
+        )     
+}
+
+export default NewsItem
